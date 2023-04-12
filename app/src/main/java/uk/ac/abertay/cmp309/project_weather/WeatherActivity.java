928bbf3c312f51771eca5003f3013b247156dc2a
LukeSweetname <2202448@uad.ac.uk>
2023-04-12 15:26:19 +0100

    Now able to call data from JSON object (main) to WeatherActivity. Both with location & currentLocationSwitch

--- app/src/main/java/uk/ac/abertay/cmp309/project_weather/WeatherActivity.java
+++ app/src/main/java/uk/ac/abertay/cmp309/project_weather/WeatherActivity.java
@@ -4,24 +4,47 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.widget.TextView;
 
+import org.json.JSONArray;
 import org.json.JSONException;
 import org.json.JSONObject;
 
 public class WeatherActivity extends AppCompatActivity {
+    TextView textViewPractice;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_weather);
 
+        textViewPractice = findViewById(R.id.textViewPractice);
+
         String jsonResponseString = getIntent().getStringExtra("json_response");
+
         try {
             JSONObject jsonResponse = new JSONObject(jsonResponseString);
 
+            // Get the location name
+            String locationName = jsonResponse.getString("name");
+
+            // Get the temperature
+            JSONObject main = jsonResponse.getJSONObject("main");
+            double temp = main.getDouble("temp") - 273.15;
+
+            // Get the weather description
+            JSONArray weather = jsonResponse.getJSONArray("weather");
+            JSONObject weatherObject = weather.getJSONObject(0);
+            String description = weatherObject.getString("description");
+
+            // Use the retrieved data
+            textViewPractice.setText("description: " + description + "\n" + "name: " + locationName + "\n" + "temp: " + temp);
+
+
         } catch (JSONException e) {
-            throw new RuntimeException(e);
+            e.printStackTrace();
         }
+
 
     }
 }
