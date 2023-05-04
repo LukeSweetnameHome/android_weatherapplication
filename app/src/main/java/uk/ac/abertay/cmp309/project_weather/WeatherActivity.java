66c1d28ab803043b72626d0c98c9b4c3e04da5db
LukeSweetname <2202448@uad.ac.uk>
2023-05-05 00:52:17 +0100

    debugged home button on WeatherActivity

--- app/src/main/java/uk/ac/abertay/cmp309/project_weather/WeatherActivity.java
+++ app/src/main/java/uk/ac/abertay/cmp309/project_weather/WeatherActivity.java
@@ -4,7 +4,6 @@ import android.content.Context;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
-import android.widget.Button;
 import android.widget.ImageView;
 import android.widget.TextView;
 
@@ -186,7 +185,6 @@ public class WeatherActivity extends AppCompatActivity implements View.OnClickLi
         }
     @Override
     public void onClick(View v) {
-        Button goHomeButton = findViewById(R.id.goHomeButton);
         Intent intent = new Intent(WeatherActivity.this, MainActivity.class);
         startActivity(intent);
     }
