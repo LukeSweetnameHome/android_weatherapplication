ba69bc3338c65e3f7b3e10a4a94ac1d1de0c2e22
LukeSweetname <2202448@uad.ac.uk>
2023-05-04 12:13:41 +0100

    added login button to registration

--- app/src/main/java/uk/ac/abertay/cmp309/project_weather/RegistrationActivity.java
+++ app/src/main/java/uk/ac/abertay/cmp309/project_weather/RegistrationActivity.java
@@ -1,25 +1,25 @@
 package uk.ac.abertay.cmp309.project_weather;
 
-import androidx.annotation.NonNull;
-import androidx.appcompat.app.AppCompatActivity;
-import android.os.Bundle;
 import android.content.Intent;
+import android.os.Bundle;
 import android.text.TextUtils;
 import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
 import android.widget.ProgressBar;
 import android.widget.Toast;
-import android.widget.EditText;
-import android.widget.TextView;
-import android.widget.Button;
 
-import com.google.firebase.auth.FirebaseAuth;
-import com.google.firebase.auth.AuthResult;
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.AppCompatActivity;
+
 import com.google.android.gms.tasks.OnCompleteListener;
 import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
 public class RegistrationActivity extends AppCompatActivity {
 
     private EditText emailTextView, passwordTextView;
-    private Button Btn;
+    private Button Btn, BtnLogin;
     private ProgressBar progressbar;
     private FirebaseAuth mAuth;
 
@@ -35,6 +35,7 @@ public class RegistrationActivity extends AppCompatActivity {
         emailTextView = findViewById(R.id.email);
         passwordTextView = findViewById(R.id.passwd);
         Btn = findViewById(R.id.btnregister);
+        BtnLogin = findViewById(R.id.btnLogin2);
         progressbar = findViewById(R.id.progressbar);
 
         // Set on Click Listener on Registration button
@@ -112,5 +113,9 @@ public class RegistrationActivity extends AppCompatActivity {
                         }
                     }
                 });
+    }
+    public void handleLogin (View v){
+        Intent intent = new Intent(RegistrationActivity.this, LoginActivity.class);
+        startActivity(intent);
     }
 }
